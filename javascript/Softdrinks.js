03b33d25f02fdf852af0d9c10875e4f6e9486ca8
BenWyss <BenWyss@github.com>
2017-12-15 12:27:40 +0100

    Softdrinks HTML & CSS
    TODO: Responsive Design

---
+++ javascript/Softdrinks.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	var request = new XMLHttpRequest();
+	request.open("GET","https://tonyspizzafactory.herokuapp.com/api/softdrinks", false);
+	request.setRequestHeader("Authorization","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4");
+	request.send();
+	
+	var parsedData = JSON.parse(request.responseText);
+
+	for(var i = 0; i < parsedData.length; i++) {
+		document.getElementsByClassName("Title")[i].innerHTML = parsedData[i].name;
+		document.getElementsByClassName("Price")[i].innerHTML = parsedData[i].prize;
+		document.getElementsByClassName("Image")[i].style.backgroundImage = "url("+parsedData[i].imageUrl+")";
+	}
+}
