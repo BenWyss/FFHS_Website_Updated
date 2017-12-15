9cdb10ab86c401a6d24e85f28942844745e766e1
BenWyss <BenWyss@github.com>
2017-12-15 12:16:06 +0100

    Salad HTML & CSS
    TODO: Responsive Design

---
+++ javascript/Salad.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	var request = new XMLHttpRequest();
+	request.open("GET","https://tonyspizzafactory.herokuapp.com/api/salads", false);
+	request.setRequestHeader("Authorization","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4");
+	request.send();
+	
+	var parsedData = JSON.parse(request.responseText);
+	
+	for(var i = 0; i < parsedData.length; i++) {
+		document.getElementsByClassName("Title")[i].innerHTML = parsedData[i].name;
+		document.getElementsByClassName("Description")[i].innerHTML = parsedData[i].ingredients.toString();
+		document.getElementsByClassName("Price")[i].innerHTML = parsedData[i].prize;
+		document.getElementsByClassName("Image")[i].style.backgroundImage = "url("+parsedData[i].imageUrl+")";
+	}
+}
